5aa77bf05cf46b0da64038d79457a84380ac5577
Matt Sampson <matt.sampson@princeton.edu>
2023-01-31 12:45:48 -0500

    added score test

--- sampling_eqx.py
+++ sampling_eqx.py
@@ -186,6 +186,9 @@ def main(
     # choose point in time to evaluate score
     t_eval = 1.0
     t = jnp.array([t_eval])
+    #t = jr.uniform(tkey, (batch_size,), minval=0, maxval=t1 / batch_size)
+    #t = t + (t1 / batch_size) * jnp.arange(batch_size)
+    t = einops.reduce(t, 'b -> () b', 'max')
     print(f'shape of t: {t.shape}')
     print(f't is: {t}')
 
